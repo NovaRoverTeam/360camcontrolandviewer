453e2b21debec18bca2ec976bfa6072f581915a9
BoredinPBL <thomas.123@live.com.au>
2017-12-11 16:04:23 +1100

    Put the unity source up for the first time
    I hope it's not too big

---
+++ Assets/Scripts/UI/CameraRotateOld.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraRotateOld : MonoBehaviour
+{
+
+   // public float minX = -360.0f;
+    //public float maxX = 360.0f;
+
+    //public float minY = -45.0f;
+    //public float maxY = 45.0f;
+
+    public float sensZ = 100.0f;
+    public float sensY = 100.0f;
+
+    float rotationY = 0.0f;
+    float rotationZ = 0.0f;
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) )
+        {
+            rotationZ += Input.GetAxis("Horizontal") * sensZ * Time.deltaTime;
+            rotationY += Input.GetAxis("Vertical") * sensY * Time.deltaTime;
+            //rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotationY, 0, rotationZ);
+        }
+    }
+}
